ba2c33f18b907a8cd7ff271b8c5c05fb1708c1fd
lilytran997 <lytran997@gmail.com>
2019-05-04 20:06:17 +0700

    finished!!

--- app/src/main/java/com/example/btvnweek4/MainActivity.kt
+++ app/src/main/java/com/example/btvnweek4/MainActivity.kt
@@ -7,17 +7,18 @@ import android.support.v7.widget.LinearLayoutManager
 import com.example.btvnweek4.Moviemodel.Movie
 import kotlinx.android.synthetic.main.activity_main.*
 
+
+const val MOVIE_TITLE_KEY = "MOVIE_TITLE_KEY"
+const val MOVIE_POSTER_KEY = "MOVIE_POSTER_KEY"
+const val MOVIE_DES_KEY = "MOVIE_DES_KEY"
+const val MOVIE_VOTE_KEY ="MOVIE_VOTE_KEY"
+const val MOVIE_BACKDROP_KEY = "MOVIE_BACKDROP_KEY"
+const val MOVIE_VIDEO_KEY ="MOVIE_VIDEO_KEY"
+const val MOVIE_DATE_KEY = "MOVIE_DATE_KEY "
 class MainActivity : AppCompatActivity(){
     var list_movie: ArrayList<Movie.Results> = ArrayList()
     lateinit var movieAdapter: MovieAdapter
-    val MovieModel: Movie()
-    val MOVIE_TITLE_KEY = "MOVIE_TITLE_KEY"
-    val MOVIE_POSTER_KEY = "MOVIE_POSTER_KEY"
-    val MOVIE_DES_KEY = "MOVIE_DES_KEY"
-    val MOVIE_VOTE_KEY ="MOVIE_VOTE_KEY"
-    val MOVIE_BACKDROP_KEY = "MOVIE_BACKDROP_KEY"
-    val MOVIE_VIDEO_KEY ="MOVIE_VIDEO_KEY"
-    val MOVIE_DATE_KEY = "MOVIE_DATE_KEY "
+    val MovieModel = Movie()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
